2cee424a9ee1e4ac3c191feb1e0d30976223adc1
Toluwani Oyewusi <toyewusi@hawk.iit.edu>
2022-09-17 01:34:48 -0500

    fixed battery

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,17 @@
+from battery.battery import Battery
+
+
+class NubbinBattery(Battery):
+  def _init_(self, last_service_date, current_date):
+    self.last_service_date = last_service_date
+    self.current_date = current_date
+
+  
+  def needs_service(self):
+    next_service_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+
+    if self.current_date > next_service_date:
+      return True
+    else:
+        return False
+
---
+++ battery/spindler_battery.py
@@ -0,0 +1,15 @@
+from battery.battery import Battery
+
+
+class SpindlerBattery(Battery):
+  def _init_(self, last_service_date, current_date):
+    self.last_service_date = last_service_date
+    self.current_date = current_date
+
+  def needs_service(self):
+    next_service_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+
+    if self.current_date > next_service_date:
+      return True
+    else:
+        return False
